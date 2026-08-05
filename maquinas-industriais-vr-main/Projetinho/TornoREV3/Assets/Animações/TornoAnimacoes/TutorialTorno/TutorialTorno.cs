4da58977708273918ad19ecb49764fe886e32389
Lorenzo Trindade <151590431+lorenzotrindade@users.noreply.github.com>
2026-08-05 16:35:51 -0300

    InserçãoScripts

--- maquinas-industriais-vr-main/Projetinho/TornoREV3/Assets/Animações/TornoAnimacoes/TutorialTorno/TutorialTorno.cs
+++ maquinas-industriais-vr-main/Projetinho/TornoREV3/Assets/Animações/TornoAnimacoes/TutorialTorno/TutorialTorno.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public class TutorialTorno : MonoBehaviour
 {
-    
-    void Start()
+    public Animator Nurbspath;
+    public Animator animCylinder011;
+
+    private int passoAtual = 0;
+    private void OnTriggerEnter(Collider other);
     {
         
+        if (other.CompareTag("Player") && passoAtual == 0) 
+        {
+            ExecutarTutorial();
+        }
     }
 
-    
-    void Update()
+    void ExecutarTutorial()
     {
+       
+        animNurbspath.SetTrigger("IniciarNurbs"); 
+        passoAtual = 1;
+
         
+        Invoke("RodarCylinder", 3.0f); 
     }
+
 }
+
